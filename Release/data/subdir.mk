7802457d9552029e88da3e4f28ac17704f9f8ede
van <463912679@qq.com>
2018-07-12 00:16:59 +0800

    删除垃圾配置

---
+++ Release/data/subdir.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+OBJ_SRCS += \
+../data/tank1215_b_m1.obj 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
